6fbfba32439e8e6fb8d296cf2cc249018aff2c49
SeekingAura <carlox216@gmail.com>
2017-02-22 16:21:50 -0500

    se retira token FACTOR

--- analizador léxico/léxico lola - 1088029924 1088338015.py
+++ analizador léxico/léxico lola - 1088029924 1088338015.py
@@ -10,7 +10,7 @@ class CalcLexer(Lexer):
 	
 	tokens = {
 		#valores
-		'identificador', 'INTEGER',  'FACTOR', 
+		'identificador', 'INTEGER',
 		#simbolos
 		'DOSPUNTOSIGUAL', 'MENORIGUAL', 'MAYORIGUAL', 'FLECHADERECHA', 'DOBLEPUNTO', "MAS", "MENOS", "MULTIPLICADOR", "IGUAL", "POTENCIA", 
 		#palabras reservadas
