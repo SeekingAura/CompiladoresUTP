783b2a3de342b4c1bd88996706514d11d83bfe0c
SeekingAura <carlox216@gmail.com>
2017-02-22 15:29:05 -0500

    Initial commit

---
+++ léxico lola - 1088029924 1088338015.py
@@ -0,0 +1,123 @@
+# -----------------------------------------------------------------------------
+# calc.py
+# -----------------------------------------------------------------------------
+
+from sly import Lexer
+
+class CalcLexer(Lexer):
+	fileName=""
+	reserved_words = { 'BEGIN', 'CONST', 'END', 'IN', 'INOUT', 'MODULE', 'OUT', 'REG', 'TS', 'OC', 'TYPE', 'VAR', 'DIV', 'MOD', 'MUX', 'LATCH', 'SR', 'IF', 'THEN', 'ELSIF', 'FOR', 'DO', 'WHILE', 'RETURN' }
+	
+	tokens = {
+		#valores
+		'identificador', 'INTEGER',  'FACTOR', 
+		#simbolos
+		'DOSPUNTOSIGUAL', 'MENORIGUAL', 'MAYORIGUAL', 'FLECHADERECHA', 'DOBLEPUNTO', "MAS", "MENOS", "MULTIPLICADOR", "IGUAL", "POTENCIA", 
+		#palabras reservadas
+		*reserved_words,
+	}
+	ignore = ' \t'
+	
+	literals = { '~', '&', '|', '#', '<', '>', '(', ')', '[', ']', '{', '}', '.', ',', ';', ':' , "'", '!', '↑'}
+	
+	
+	
+	#Tokens - otros
+	#Tokens - operadores
+	"""
+	#forma de sobre-escribir token apartir de solo una expresión regular, puede estar sin definirse el token
+	@_(r'\+')
+	def PLUS(self, t):
+		t.type = 'PLUS'      # Set token type to the expected literal
+		return t
+	"""
+	#@_ es un llamado por parte d ela clase por su patron decorador
+	@_(r'[a-zA-Z_][a-zA-Z0-9_]*')
+	def identificador(self, t):
+		#Control de palabras reservadas, o ma bien de priorizar check de expresión regular
+		# Chec	k if name matches a reserved word (change token type if true)
+		if t.value.upper() in self.reserved_words:
+			t.type = t.value.upper()
+		return t
+	
+	
+	@_(r'\d+')#expresión regular que trabaja [0-9]
+	def INTEGER(self, t):
+		t.value = int(t.value)
+		return t
+	"""
+	@_(r'\d+|[0-9a-fA-F]+H')
+    def INTEGER(self, t):
+        a=str(t.value)
+        if(a[len(a)-1]== 'H'):#manejo hexa
+            t.value = int (str(t.value), 16)
+        else:
+            t.value = int (str(t.value), 10)
+        return t
+	"""
+	#othres ignore
+	@_(r'\n+')
+	def ignore_NEWLINE(self, t):
+		self.lineno += t.value.count('\n')
+	#r'\(\*([^*)]|\*[^*]\).*)*\*\)' expresion mejorada
+	ignore_COMMENT=r'\(\*([^*]|\*[^)]|\))*\*\)'#\(\*[^*)\n]*\*\) sin saltos de linea, anterior \(\*[^*)]*\*\), 
+
+
+		
+	# Compute column.
+	#     input is the input text string
+	#     token is a token instance
+	def getColumn(self, token):
+		last_cr = self.text.rfind('\n', 0, token.index)+1
+		#print("last_cr {}".format(last_cr))#verificando valor de indexados previos
+		if last_cr < 0:
+			last_cr = 0
+		column = (token.index - last_cr) + 1
+		return column
+	#Tokens - simbolos compuestos
+	DOBLEPUNTO = r'\.\.'#r'[.][.]'
+	FLECHADERECHA = r'->'
+	MAYORIGUAL = r'>='
+	MENORIGUAL = r'<='
+	DOSPUNTOSIGUAL = r':='
+	#Tokens - otros simbolos sencillos
+	MAS = r'\+'
+	MENOS = r'-'
+	MULTIPLICADOR = r'\*'
+	IGUAL = r'='
+	POTENCIA = r'\^'
+	
+	#error control
+	@_(r'\(\*([^*]|\*[^)]|\))*')
+	def error_COMMENT(self, t):
+		print('File "{}" Line {} Colum {}'.format(self.fileName, t.lineno, self.getColumn(t)))
+		print(t.value)
+		print("ERROR - No terminó comentario")
+		print("Se esperaba un *)")
+	def error(self, value):
+		print("Illegal character {}".format(value[0]))
+		self.index += 1
+		
+if __name__ == '__main__':
+	import sys
+	lexer = CalcLexer()
+	if(len(sys.argv)!=2):#Verifica la cantidad de argumentos a la hora de compilar si no son 2. "py 'fichero.py' 'archivo'"
+		sys.stderr.write('Usage: "{}" "filename"\n'.format(sys.argv[0]))#permite que al al compilar indique que debe de darse el archivo de la forma python.exe "fichero.py" "Archivo a abrir, como un simple print"
+		raise SystemExit(1)#termina el programa
+	file= open(sys.argv[1]).read()#en caso de que indique archivo tome el segundo string y abra el archivo y convierta con .read() a string
+	maxLenthLine=0
+	for linea in open(sys.argv[1]):
+		if(maxLenthLine<len(linea)):
+			maxLenthLine=len(linea)#verifica la columna maxima para el formato de centrado (visual en los print)
+			print("malen",maxLenthLine)
+	print("{:{align}{width}}".format("Archivo recibido - contenido inicio", align="^", width=maxLenthLine))
+	print("-"*maxLenthLine)
+	print(file)
+	print("-"*maxLenthLine)
+	print("{:{align}{width}}".format("Archivo recibido - contenido fin", align="^", width=maxLenthLine))
+	
+	#print("aplicando tokenize")
+	lexer.fileName=sys.argv[1]
+	for tok in lexer.tokenize(file):
+		print("Linea {}, columna {}, indexado {}".format(tok.lineno, lexer.getColumn(tok), tok.index))
+		print('tipo {} valor {}'.format(tok.type, tok.value))
